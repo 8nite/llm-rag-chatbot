e4f4e1b4778ca7a52024326834031f20a0a5ad3f
Herbert Tung <herberttung@hotmail.com>
2024-09-18 09:56:02 -0400

    contactus

--- src/pages/api/email.js
+++ src/pages/api/email.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     if (!from || !subject || !message) {
       return res
         .status(400)
-        .json({ error: "Missing required fields: to, subject, message" });
+        .json({ error: "Missing required fields: from, subject, message" });
     }
 
     try {
